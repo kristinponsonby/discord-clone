19f79b4d84917352979524c5e544987dccf25eb6
Kristin Ponsonby <kristin.ponsonby@dispensaryofhope.org>
2022-09-09 17:26:27 -0400

    Begin building chat component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
+import Chat from './Chat';
 import Sidebar from './Sidebar';
 
 function App() {
   return (
-    <div className="App">
-    {/*Sidebar*/}
+    <div className="app">
     <Sidebar />
-    
-    {/*Chat*/}
+    <Chat />
     </div>
   );
 }
---
+++ src/ChatHeader.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './ChatHeader.css';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import EditLocationIcon from '@mui/icons-material/EditLocation';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import SearchIcon from '@mui/icons-material/Search';
+import SendIcon from '@mui/icons-material/Send';
+import HelpIcon from '@mui/icons-material/Help';
+
+function ChatHeader() {
+  return (
+    <div className="chatHeader">
+        <div className="chatHeader__left">
+            <h3>
+                <span className="chatHeader__hash">
+                    #
+                </span>
+                Test Channel Name
+            </h3>
+        </div>
+
+        <div className="chatHeader__right">
+            <NotificationsIcon />
+            <EditLocationIcon />
+            <PeopleAltIcon />
+
+            <div className="chatHeader__search">
+                <input placeholder="Search" />
+                <SearchIcon />
+            </div>
+            <SendIcon />
+            <HelpIcon />
+        </div>
+    </div>
+  )
+}
+
+export default ChatHeader
