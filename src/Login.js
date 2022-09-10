e3c5c50afe0281d07fb95d9b13702ba49b556857
Kristin Ponsonby <kristin.ponsonby@dispensaryofhope.org>
2022-09-10 13:55:09 -0400

    Remove login dummy text

--- src/Login.js
+++ src/Login.js
@@ -12,7 +12,6 @@ function Login() {
 
   return (
     <div className="login">
-    <h2>I am the Login page</h2>
     <div className="login__logo">
         <img src ="https://cdn.worldvectorlogo.com/logos/discord-logo-color-wordmark-1.svg"
         alt="Discord logo" width="60%" align="center" />
